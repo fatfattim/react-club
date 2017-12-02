15e5f86d532c2a2499a3587965b5d4d230d191b0
TimChen <timchen@kkstream.com.tw>
2017-12-02 17:11:28 +0800

    Add two exams codes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
-class HelloMessage extends React.Component {
+
+//Exam 1
+class Greeting extends React.Component {
   render() {
     return (
-      <div>
-        Hello {this.props.name}
-      </div>
+      <h1>Hello, {this.props.name}</h1>
     );
   }
 }
+
+Greeting.propTypes = {
+  //Exam 1
+  name: PropTypes.string,
+
+  //Exam 2
+  customProp: function(props, propName, componentName) {
+    if (props[propName] !== "Tim is so handsome") {
+      return new Error(
+        'Invalid prop `' + propName + '` supplied to' +
+        ' `' + componentName + '`. Validation failed.'
+      );
+    }
+  }
+};
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+      console.log('Test');
+    }
   render() {
     return (
       <div className="App">
@@ -21,7 +47,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <HelloMessage name="TimTim"/>
+        <Greeting name={123} customProp="^__^"/>
       </div>
     );
   }
